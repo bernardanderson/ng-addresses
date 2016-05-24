b1360bf1693c08cddfffc9a99532e6ec7e2c100d
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-24 09:09:06 -0500

    Added XHR GET call and clearing of Array =>

--- app/controllers/listContactsController.js
+++ app/controllers/listContactsController.js
@@ -1,6 +1,8 @@
 "use strict";
 
-app.controller("listContactsController", function($scope, AddressListService){
+app.controller("listContactsController", function($scope, XHRCalls, AddressListService){
 
+  AddressListService.clearAddressArray();
+  XHRCalls.getAddresses("");
   $scope.addressList = AddressListService.currentAddresses;
 });
--- app/factories/firebaseFactories.js
+++ app/factories/firebaseFactories.js
@@ -1,21 +1,43 @@
+"use strict";
 
 // This factory allows the storing and retrieval of the addresses across all controllers
 app.factory('AddressListService', function(){
 
   return {
 
-    currentAddresses: [
-      {
-        name: "Bernie Anderson",
-        address: "Home",
-        telephone: "615-911-4111",
-        email: "junk@hotmail.com"
-      }
-    ],
+    currentAddresses: [],
+
+    clearAddressArray: function() {
+      this.currentAddresses.splice(0);
+    },
 
     update: function(sentNewAddressObject) {
       this.currentAddresses.push(sentNewAddressObject);
     }
   };
 
+});
+
+app.factory('XHRCalls', function($q, $http, AddressListService){
+
+  return {
+
+    getAddresses: function(sentID) {
+
+      return $q(function(resolve, reject){
+
+        $http.get(`https://ba-addressbook.firebaseio.com/addressBook/${sentID}.json`)
+        .success(function(addressCollection){
+          Object.keys(addressCollection).forEach(function(key){
+            addressCollection[key].id = key;
+            AddressListService.update(addressCollection[key]);
+          });
+          resolve();
+        })
+        .error(function(error) {
+          reject(error);
+        });
+      });
+    }
+  };
 });
