b5b0cf674ab4858626427aff7fe927111b842ffd
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-26 14:35:52 -0500

    Sorting of the Contact attributes is completed The $filter and orderBy method was used to sort the current list of contacts.  When the sort double arrows are clicked that triggers the sort function.

--- app/controllers/listContactsController.js
+++ app/controllers/listContactsController.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("listContactsController", function($scope, XHRCalls, AddressListService){
+app.controller("listContactsController", function($scope, $filter, XHRCalls, AddressListService){
 
   let listAddress = function() {
       AddressListService.clearAddressArray();
@@ -12,6 +12,22 @@ app.controller("listContactsController", function($scope, XHRCalls, AddressListS
     let deleteItemID = sentEvent.currentTarget.attributes['fb-id'].value;
     XHRCalls.xhrAddresses(deleteItemID, "delete");
     AddressListService.deleteAddressArrayItem(deleteItemID);
+  };
+
+  $scope.sortItem = function(sentSortVar) {
+
+    $scope.addressList = AddressListService.currentAddresses; // Gets the current array of addresses
+
+    // Checks to see if one of the sort buttons was already clicked.  If not, the reverse gets set to false and sets the predicate
+    //   to the new sort parameter.  If true, the reverse gets set to true.
+    if ($scope.predicate === sentSortVar) {
+      $scope.reverse = !$scope.reverse; // Sets the opposite of what .reverse is already set
+    } else {
+      $scope.predicate = sentSortVar; // Sets the sorting item that the user clicked
+      $scope.reverse = false;         // Sets reverse to false since it's the first time that sort item was clicked
+    }
+
+    $scope.addressList = $filter('orderBy')($scope.addressList, sentSortVar, $scope.reverse); // Sets the array to the sorting params
   };
 
   listAddress();
